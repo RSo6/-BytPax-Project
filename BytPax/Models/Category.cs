52ad13e408be7be41d82c64573d840d757795568
Roman Shnep <128533249+RSo6@users.noreply.github.com>
2025-03-17 20:51:14 +0100

    feat: Add Category Class(Model)
    Added Category Class in directory Models according to MVC pattern, to operate with essential entities.
    
    task #2

---
+++ BytPax/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace BytPax.Models;
+
+public class Category  {
+    public int Id { get; set; }
+    public string TypeOfSport { get; set; }
+    public string description { get; set; }
+    
+}
--- BytPax/Models/User.cs
+++ BytPax/Models/User.cs
@@ -1,10 +1,10 @@
 namespace BytPax.Models;
 
 public class User  {
-    public int Id { get; private set; }
-    public string Full_name { get; private set; }
-    public string Image_path { get; private set; }
+    public int    Id { get; private set; }
+    public bool   TypeOfUser { get; private set; }
+    public string FullName { get; private set; }
+    public string ImagePath { get; private set; }
     public string Email { get; private set; }
-    public bool Type_of_user { get; private set; }
     public string Password { get; private set; }
 }
