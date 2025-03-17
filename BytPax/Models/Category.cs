8a0450211da6340d260e71706fae5b98e7934cda
Roman Shnep <128533249+RSo6@users.noreply.github.com>
2025-03-17 21:05:00 +0100

    fix: Update Category Class(Model)
    Added new entities "Name" of category
    
    task #2

--- BytPax/Models/Category.cs
+++ BytPax/Models/Category.cs
@@ -2,6 +2,7 @@ namespace BytPax.Models;
 
 public class Category  {
     public int Id { get; set; }
+    public string Name { get; set; }
     public string TypeOfSport { get; set; }
     public string Description { get; set; }
     
