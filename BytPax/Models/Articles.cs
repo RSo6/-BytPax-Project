2bbea7fb319956b8855bff4c72ba50f576613f78
Roman Shnep <128533249+RSo6@users.noreply.github.com>
2025-03-17 21:02:31 +0100

    feat: Add Athletes Class(Model)
    Added Athletes Class in directory Models according to MVC pattern, to have ability get full possible information about athletes.
    
    task #2

--- BytPax/Models/Articles.cs
+++ BytPax/Models/Articles.cs
@@ -4,7 +4,7 @@ public class Articles {
     public int Id { get; set; }
     public string Topic { get; set; }
     public string BodyText { get; set; }
-    public string CategoryId { get; set; }
+    public int CategoryId { get; set; }
     public DateTime Date { get; set; } = DateTime.UtcNow;
     public string  ImagePath {get; set;}
 }
---
+++ BytPax/Models/Athletes.cs
@@ -0,0 +1,27 @@
+namespace BytPax.Models;
+
+public class Athletes {
+    public int Id { get; set; }
+    public int Age { get; set; }
+    public int CategoryId { get; set; }
+    public string FullName { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string Description { get; set; }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
